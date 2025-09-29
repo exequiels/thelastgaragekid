020fa76f20d341eb3b17ceb69b5e5ea26e3aecc6
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2025-09-29 14:23:22 -0300

    no message

--- views/pages/wiki.php
+++ views/pages/wiki.php
@@ -1,6 +1,8 @@
 <div class="border-garage shadow-garage">
   <div class="p-2 m-1 border-contenedores bg-white"><h2 class="fs-6 mt-2">WIKI</h2></div>
-  <div class="p-2 m-1 ms-2">Basically thelastgaragekid project readme from my repo.</div>
+  <div class="p-2 m-1 ms-2">Basically thelastgaragekid project readme from my       
+      <a href="https://github.com/exequiels/thelastgaragekid" target="_blank" rel="noopener">
+        repo</a>. I'm going to be updating this, it's not complete it's a work in progress as long as I add and add content, ideas, etc.</div>
 </div>
 
 <div class="border-garage shadow-garage mt-3">
